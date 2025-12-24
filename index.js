4fd1a61234575c04ef745110efaa9c359a8213bb
vatsal <88679701+vatsalost@users.noreply.github.com>
2025-12-25 01:05:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import { Client, GatewayIntentBits } from "discord.js";
+import express from "express";
+
+const app = express();
+app.get("/", (req, res) => res.send("Alive"));
+app.listen(3000, () => console.log("Keep-alive server running"));
 
 const client = new Client({
   intents: [
@@ -8,9 +13,18 @@ const client = new Client({
   ]
 });
 
-client.on("messageCreate", (message) => {
+client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
-  message.reply("I can see messages 👀");
+
+  console.log("MESSAGE:", message.content);
+
+  if (!message.content.toLowerCase().startsWith("disha")) return;
+
+  await message.reply("Haan bolo 😌");
+});
+
+client.once("ready", () => {
+  console.log(`Disha online as ${client.user.tag}`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
